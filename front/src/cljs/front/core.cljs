184aa65d75b969ae70f53ba60efdb158494fde72
tommy-mor <thmorriss@gmail.com>
2018-06-10 21:22:20 -0700

    made ac box pop up at the right time

--- front/src/cljs/front/core.cljs
+++ front/src/cljs/front/core.cljs
@@ -1,7 +1,7 @@
 (ns front.core
   (:require
-   [reagent.core :as reagent]
-   ))
+   [reagent.core :as reagent]))
+   
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -25,7 +25,6 @@
 (defn scroll-to-bottom []
   (let [a (.getElementById js/document "back")]
     (set! (.-scrollTop a) 9999999)))
-
 (defn send-message [message]
   (swap! counter inc)
   (reset! messages (conj @messages message))
@@ -33,9 +32,8 @@
   (scroll-to-bottom))
 
 (defn getstub [textbox]
-  (let [a (re-find #"@[^\s]*" (str textbox))]
-    (println a "ssssss")
-    a))
+  (let [a (clojure.string/join (rest (re-find #"@[^\s]*" (str textbox))))]
+    (println a "ssssss")  a))
 
 (defn get-cursor-pos []
   (let [a (.getElementById js/document "text")
@@ -47,22 +45,28 @@
 (defn auto-complete-box [textbox]
   (let [selected (filter #(re-find (re-pattern (getstub textbox)) %) @list-of-ppl)]
     [:div
-     (if (= (get textbox (- (get-cursor-pos) 1)) "@")
-       [:div.autobox [:pre (for [item selected] (str item "\n"))]])]))
+     (let [frontindex (clojure.string/index-of textbox "@")]
+       ;; check if there are any spaces in the substring between @ and the cursor, indicating that we should
+       ;; not show the box
+       (if-not (and frontindex (not (clojure.string/includes? (subs textbox frontindex (get-cursor-pos)) " ")))
+        (println (str "here " (subs textbox frontindex (get-cursor-pos))))
+        [:div.autobox [:pre (for [item selected] (str item "\n"))]]))]))
 
 (defn message-list [messages]
   [:div
    (for [item messages]
      (do
-       ^{:key (:key item)} [:div (:text item) ]))])
+       ^{:key (:key item)} [:div (:text item)]))])
 
 (defn interp [one]
-    one)
+
+  one)
 
 (defn page []
   (fn []
     (let [complete #(swap! textbox interp)]
       [:div#back.mainscreen
+       [:div (getstub @textbox)]
        [message-list @messages]
        [:div.textinput
         [:input#text.textinput {:auto-focus true :type "text"
@@ -72,8 +76,8 @@
                                                 13 (send-message {:text @textbox :key @counter})
                                                 39 (complete)
                                                 nil)}]]
-       [auto-complete-box @textbox]
-       ])))
+       [auto-complete-box @textbox]])))
+
 
 
 
@@ -83,8 +87,8 @@
 (defn dev-setup []
   (when ^boolean js/goog.DEBUG
     (enable-console-print!)
-    (println "dev mode")
-    ))
+    (println "dev mode")))
+
 
 (defn reload []
   (reagent/render [page]
